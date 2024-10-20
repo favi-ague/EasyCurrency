0841caa1eb5eaaddbfe2d3d2e973a49edfb49ac5
favi-ague <faviague02@gmail.com>
2024-10-20 14:28:45 +0300

    first commit

---
+++ src/test/java/com/project/easycurrency/EasycurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.easycurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasycurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
